b226a25ea1d2b7f2cddd35ab719e1bec132002f5
merchantsons <160190585+merchantsons@users.noreply.github.com>
2024-12-08 19:24:43 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily:{
+        poppins:['Poppins'],        
+        GreyQo:['Grey Qo'],
+        Qwitcher_Grypen:['Qwitcher Grypen'],
+        Great_Vibes:['Great Vibes'],
+        Cinzel:['Cinzel'],
+        Satoshi:['Satoshi'],
+      },
+      screens: {
+        'sm': '320px',
+        // => @media (min-width: 320px) { ... }
+        
+        'md': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },          
+    },
+  },
+  plugins: [],
+};
+export default config;
